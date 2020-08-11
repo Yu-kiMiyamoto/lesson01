8126dd2b99de4afec37131b3f91b6bd17ecf9fb9
宮本優輝 <yk.38m.svt@gmail.com>
2020-08-11 22:28:15 +0900

    4-1 **コマンドライン引数で5つの整数値を入力させ、5つの値のうち3番目に大きい値を表示するプログラムを作成せよ**

---
+++ 4-1/main.js
@@ -0,0 +1,18 @@
+const arg = process.argv
+
+//コマンドライン引数から、配列を作る
+const array = [];
+
+for ( let i = 2; i < arg.length; i++ ){
+    array.push(arg[i])
+}
+
+
+//小さい順に並べる
+array.sort(function(a,b){
+    return a - b;
+});
+console.log(array);
+
+//３番目の値を取得
+console.log(array[2]);
