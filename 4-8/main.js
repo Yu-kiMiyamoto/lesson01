340b2216bb6fc7cc98be41a7f6481fd0d23447a5
宮本優輝 <yk.38m.svt@gmail.com>
2020-08-19 00:21:55 +0900

    4-8

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,11 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+
+//文字列strを"はるこ"、"こはる"で区切って配列を作る
+const haruko = str.split("はるこ");
+const koharu = str.split("こはる");
+//"はるこ"、"こはる"で区切った配列の個数から１を引いた数が"はるこ"、"こはる"の個数
+const harukoCount = haruko.length - 1;
+const koharuCount = koharu.length - 1;
+
+console.log(`はるこ:${harukoCount}`);
+console.log(`こはる:${koharuCount}`);
